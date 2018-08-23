ef0517422aeda441a940b910186877cbe721bd2b
Vergil Kelley <verg_1@yahoo.com>
2018-08-22 22:13:45 -0500

    first commit

---
+++ header.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<nav>
+			<div class="main-wrapper">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+				</ul>
+				<div class="nav-login">
+					<?php
+
+					
+						if (isset($_SESSION['u_id'])) {
+							
+						} else {
+							echo '<form action="includes/login.inc.php" method="POST">
+								<input type="text" name="uid" placeholder="Username/email">
+								<input type="password" name="pwd" placeholder="password">
+								<button type="submit" name="submit">Login</button>
+								</form>
+								<a href="signup.php">Sign Up</a>';
+								echo '<form action="includes/logout.inc.php" method="POST">
+									<button type="submit" name="submit">Logout</button>
+									</form>';
+							}
+					?>
+				</div>
+			</div>
+		</nav>
+	</header>
