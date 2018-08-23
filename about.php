61a0caadadffc8bc2499e58010e2f4d903fe6417
Vergil Kelley <verg_1@yahoo.com>
2018-08-22 22:19:39 -0500

    first commit

---
+++ about.php
@@ -0,0 +1,32 @@
+<?php
+  include_once 'header.php';
+?>
+
+            <div class="container">
+          <div class="row">
+            <div class="col-sm-10" id="top-photo">
+              <img src="img/v-Pierce.jpg">
+              <p style="color: #000;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+              proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      <div class="container-fluid" style="padding: 0;">
+        <div class="row">
+          <div class="col-sm-12">
+            <footer id="footer2">
+              <p ><a href="https://vergilkellley.github.io/" target="_blank">Vergil Kelley Web Design</a> &copy; 2018</p>
+            </footer>
+          </div>
+        </div>
+      </div>
+
+</body>
+</html>
