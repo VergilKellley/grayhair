e75bb09db82ed2f81436413703566fc78450ce6a
Vergil Kelley <verg_1@yahoo.com>
2018-08-23 22:45:26 -0500

    added text and photo

--- about.php
+++ about.php
@@ -2,31 +2,42 @@
   include_once 'header.php';
 ?>
 
-            <div class="container">
+        <div class="main-container">
           <div class="row">
-            <div class="col-sm-10" id="top-photo">
+            <div class="col-sm-6" id="top-photo">
               <img src="img/v-Pierce.jpg">
+
               <p style="color: #000;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo  consequat. Duis aute irure dolor in reprehenderit in voluptate 
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non velit esseproident, sunt in culpa qui officia deserunt mollit anim id est laborum. adipisicing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo  consequat. Duis aute irure dolor in reprehenderit in voluptate 
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non velit esseproident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+             </p>
+            </div>
+          </div>
+          <div class="col-sm-3" id="top-box">
+              <img id="box-photo" src="img/curlygrey.jpg">
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
               tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
               quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
               consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
               cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
               proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            </div>
           </div>
+
         </div>
-      </div>
 
 
-      <div class="container-fluid" style="padding: 0;">
+
+     <!-- <div class="container-fluid" style="padding: 0;">
         <div class="row">
           <div class="col-sm-12">
             <footer id="footer2">
-              <p ><a href="https://vergilkellley.github.io/" target="_blank">Vergil Kelley Web Design</a> &copy; 2018</p>
+              <a href="">Vergil Kelley Web Design & Development &copy; 2018</a>
             </footer>
           </div>
         </div>
-      </div>
+      </div>-->
 
 </body>
 </html>
