12c67828068e6956c664944bd0cb820a3525cf5d
Vergil Kelley <verg_1@yahoo.com>
2018-08-27 21:47:49 -0500

    styled top photo, text and nav

--- about.php
+++ about.php
@@ -1,34 +1,37 @@
 <?php
   include_once 'header.php';
 ?>
-
-        <div class="main-container">
+      
+        <div class="container" id="first-section">
           <div class="row">
-            <div class="col-sm-6" id="top-photo">
-              <img src="img/v-Pierce.jpg">
-
+            <div class="col-sm-6">
+              <img id="first-photo" src="img/v-Pierce.jpg">
               <p style="color: #000;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
               tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo  consequat. Duis aute irure dolor in reprehenderit in voluptate 
               cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non velit esseproident, sunt in culpa qui officia deserunt mollit anim id est laborum. adipisicing elit, sed do eiusmod
               tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo  consequat. Duis aute irure dolor in reprehenderit in voluptate 
-              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non velit esseproident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non velit esseproident, ssssssssssunt in culpa qui officia deserunt mollit anim id est laborum.ssssssssssunt in culpa qui officia deserunt mollit anim id est laborum.
              </p>
             </div>
           </div>
-          <div class="col-sm-3" id="top-box">
-              <img id="box-photo" src="img/curlygrey.jpg">
+        
+          <div class="col-sm-3 " id="second-section">
+              <img id="second-photo" src="img/curlygrey.jpg">
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
               tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
               quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
               consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-              proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.$$$$$$$</p>
           </div>
-
         </div>
+      
 
 
-<?php
-  include_once 'footer.php';
-?>
-     
+
+    
+        
+          <?php include_once'footer.php';?>
+        
+      
+
+
