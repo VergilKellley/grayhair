e9cceff6ff4fdec7e527d4d84b057050679e4e84
Vergil Kelley <verg_1@yahoo.com>
2018-08-27 21:45:31 -0500

    revised link

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <meta name="keywords" content="">
   <meta name="author" content="Vergil D. Kelley">
 
-  <link href="css/styles.css" type="text/css" rel="stylesheet"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet"/>
 
    <!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
